ee8f5b1492547757189305886c8976972079b089
Andrew Barrett-Sprot <abs1g14@soton.ac.uk>
2016-03-24 02:29:42 +0000

    Add functionality.

--- githubfav/commitparser.py
+++ githubfav/commitparser.py
@@ -4,14 +4,49 @@ import re
 class CommitParser:
     # Regex to match everything past the last dot
     RE_FILE_ENDINGS = re.compile(r"^.*\.(.*?)$")
+    RE_DIR_ENDINGS = re.compile(r"^.*/(.*?)$")
 
     @staticmethod
     def get_files(commit: dict):
         return commit["files"]
 
     @staticmethod
+    def get_file_ending(file):
+        match = re.match(CommitParser.RE_FILE_ENDINGS, file)
+        if match is None:
+            # if the file has no extension, try getting the file name itself
+            match = re.match(CommitParser.RE_DIR_ENDINGS, file)
+            if match is None:
+                # Otherwise the file must be a lone file in the root directory. return it.
+                return file
+            else:
+                return match.group(1)
+        else:
+            return match.group(1)
+
+    @staticmethod
     def get_file_endings(files):
         names = [x["filename"] for x in files]
-        endings = [re.match(CommitParser.RE_FILE_ENDINGS, x).group(1) for x in names]
+
+        endings = [CommitParser.get_file_ending(x) for x in names]
         return endings
 
+    @staticmethod
+    def get_mode_endings(list_of_endings):
+        """
+        Get the most common file endings
+        :param list_of_endings:
+        :return: lists of the file endings which appear the most (will be a list if there's more than 1)
+        """
+        freq = {}
+        highest = 0
+        for ending in list_of_endings:
+            if ending not in freq:
+                newfreq = 1
+            else:
+                newfreq = freq[ending] + 1
+            freq[ending] = newfreq
+            highest = max(newfreq, highest)
+
+        # Get all the line endings with the same frequency as the most frequent.
+        return [x for x, y in freq.items() if y == highest]
--- githubfav/githubfav.py
+++ githubfav/githubfav.py
@@ -1,26 +1,83 @@
 import github3
+from github3 import login
 from .commitparser import CommitParser
+from .languages import extension_to_language
 
 
-class GitHubFav():
+class GitHubFav:
+    class NotLoggedInException(Exception):
+        pass
+
+    class HTTPException(Exception):
+        pass
+
+    # How many commits of the user to check.
+    COMMIT_COUNT = 50
+    COMMITS_PER_PUSH = 3
+
     def __init__(self, github3_login: github3.GitHubEnterprise):
-        self.gh = github3_login
+        if github3_login:
+            self.gh = github3_login
+            try:
+                # Check for if the login succeeded
+                self.logged_in_user = self.gh.user()
+            except github3.models.GitHubError as e:
+                if e.code == 401:
+                    raise self.NotLoggedInException("Bad Login!")
+        else:
+            self.gh = login(username="invalid",password="invalid")
 
-    def get_commit_list(self, user):
-        commits = []
+    def get_push_list(self, user):
         user = self.gh.user(login=user)
-        for event in filter(lambda x: x.type == "PushEvent", user.iter_events()):
-            commits = commits + event.payload["commits"]
-        return commits
+        # If the user doesn't exist
+        if user is None:
+            return None
+        return filter(lambda x: x.type == "PushEvent", user.iter_events())
 
     def get_commit(self, commit_dict):
         url = commit_dict["url"]
         # Do a bit of API wrangling, as the github3 api doesn't support loading individual commits
-        print("Getting commit")
         response = self.gh._get(url)
+        if response.status_code > 400:
+            print("x", end="", flush=True)
+            raise self.HTTPException("Error when getting commit")
+        print(".", end="", flush=True)
         return response.json()
 
+    @staticmethod
+    def get_favourite_lang(list_of_endings):
+        freq = {}
+        for ending in list_of_endings:
+            if ending not in freq:
+                freq[ending] = 1
+            else:
+                freq[ending] += 1
+
+        return sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
     def get_all_commits(self, user):
-        commits = self.get_commit_list(user)
-        for commit in commits:
-            print(CommitParser.get_file_endings(CommitParser.get_files(self.get_commit(commit))))
+        pushes = self.get_push_list(user)
+        if pushes is None:
+            return None
+
+        most_common_endings = []
+
+        for push in pushes:
+            commits = push.payload["commits"][:self.COMMITS_PER_PUSH]
+            endings = []
+            for commit in commits:
+                try:
+                    endings += CommitParser.get_file_endings(
+                        CommitParser.get_files(
+                            self.get_commit(commit)
+                        )
+                    )
+                except self.HTTPException:
+                    pass
+            print("|", end="", flush=True)
+            # Most common endings
+            most_common_endings += CommitParser.get_mode_endings(endings)
+
+        print()
+        languages = [(extension_to_language(x)) for x in most_common_endings]
+        return self.get_favourite_lang(languages)
